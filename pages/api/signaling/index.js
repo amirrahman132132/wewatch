baeb085991ab57f72f0090d6a78e963230203302
amirrahman132132 <52195981+amirrahman132132@users.noreply.github.com>
2023-12-19 09:26:57 +0600

    cvb

--- pages/api/signaling/index.js
+++ pages/api/signaling/index.js
@@ -1,7 +1,7 @@
 import { signal_system_server } from "../../../scripts/signal_system/signal_system_server"
 
 const signal = signal_system_server({
-    pollinterval : 5000
+    pollinterval : 8000
 })
 
 export default async function signaling(req, res) {
--- scripts/signal_system/signal_system_server.js
+++ scripts/signal_system/signal_system_server.js
@@ -1,7 +1,7 @@
 export function signal_system_server(options = {}) {
     let obj = {}
     obj.signals = []
-    obj.pollinterval = 8000
+    obj.pollinterval = 5000
 
     obj.signalCallbacks = new Set([])
 
