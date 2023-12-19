c906a1facefc4178c26a0231cef4d7f5636ee3e4
amirrahman132132 <52195981+amirrahman132132@users.noreply.github.com>
2023-12-20 00:08:44 +0600

    ll

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,12 +20,12 @@ export default function Home() {
             <div className="root bg-yellow-100 flex w-full min-h-screen items-center justify-center">
                 <div className="flex flex-col bg-white items-center gap-6 p-20 shadow-xl font-bold">
                     <div className="text-4xl text-center leading-snug">
-                        Wellcome To <span className="text-purple-600">WeWatch updated</span>
+                        Wellcome To <span className="text-purple-600">WeWatch plll</span>
                     </div>
                     {
-                        <a href={`./room/${generateRandomString(6)}`}>
-                            <Button>Create Room</Button>
-                        </a>
+                        // <a href={`./room/${generateRandomString(6)}`}>
+                        //     <Button>Create Room</Button>
+                        // </a>
                     }
                 </div>
             </div>
