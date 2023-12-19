8b90a1e1d1ed92429425607f18e835f94a315a4b
amirrahman132132 <52195981+amirrahman132132@users.noreply.github.com>
2023-12-20 00:34:24 +0600

    poimm

--- pages/room/[room_id].jsx
+++ pages/room/[room_id].jsx
@@ -9,6 +9,10 @@ export default function Room(props) {
 }
 
 export const getServerSideProps = async (ctx) => {
+    // Set CORS headers to allow requests from any origin
+    ctx.res.setHeader('Access-Control-Allow-Origin', '*');
+    ctx.res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    ctx.res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     return {
         props: ctx.query
     }
