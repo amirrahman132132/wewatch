0e2f6ab0171c17d558333ef52ebcd4de6804ac66
amirrahman132132 <52195981+amirrahman132132@users.noreply.github.com>
2023-12-19 09:37:51 +0600

    oigh

--- next.config.js
+++ next.config.js
@@ -3,18 +3,3 @@ const nextConfig = {
   reactStrictMode: true,
 }
 module.exports = nextConfig
-// module.exports = {
-//     async headers() {
-//         return [
-//           {
-//             source: '/:path*', // Match all routes
-//             headers: [
-//               {
-//                 key: 'Cache-Control',
-//                 value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-//               },
-//             ],
-//           },
-//         ];
-//       },
-// }
