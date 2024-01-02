758dffe90426a5e68e75ac86239fad67d7a86812
amirrahman132132 <52195981+amirrahman132132@users.noreply.github.com>
2024-01-02 16:11:11 +0600

    623bb

--- src/app/api/signaling/route.js
+++ src/app/api/signaling/route.js
@@ -1,10 +1,10 @@
 import { signal_system_server } from "../../../../scripts/signal_system/signal_system_server"
 
-const signal = signal_system_server({
+global.signal = signal_system_server({
     pollinterval : 5000
 })
 
 export async function POST(req){
-    const res = await signal.handleRequest(req)
+    const res = await global.signal.handleRequest(req)
     return new Response(JSON.stringify(res))
 }
